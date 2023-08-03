304fea352780e2fefc6fa9228f9aef87a1cf4baf
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-08-03 22:36:34 +0700

    make instagram

---
+++ lesson53_redesign_instagram/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:lesson53_redesign_instagram/pages/following.dart';
+import 'package:lesson53_redesign_instagram/pages/members.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FollowingPage(),
+    ),
+  );
+}
