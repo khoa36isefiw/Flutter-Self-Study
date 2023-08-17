16b2035e5f6b51965de3250454b61d422e30e30e
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-08-17 18:31:53 +0700

    update for login page

--- lesson61_register_with_phone/README.md
+++ lesson61_register_with_phone/README.md
@@ -83,3 +83,90 @@ Create Input Phone Number
         },
     ),
   ```
+
+
+### Login Screen
+[<img src="assets/screenshot/img_login_side_1.jpg" width="250">](assets/screenshot/img_login_side_1.jpg)
+[<img src="assets/screenshot/img_login_side_2.jpg" width="250">](assets/screenshot/img_login_side_2.jpg)
+[<img src="assets/screenshot/img_login_side_3.jpg" width="250">](assets/screenshot/img_login_side_3.jpg)
+- Display another image during 5. With the code below
+```
+ int activeIndex = 0;
+
+  @override
+  void initState() {
+    //Timer.periodic is a class used to create a periodic timer.
+    // It allows you to perform an action
+    //after a certain amount of time and repeat it periodically.
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      // after 5 seconds increase 1
+      setState(() {
+        activeIndex++;
+        // count up to 4 => reset
+        if (activeIndex == 4) activeIndex = 0;
+      });
+    });
+
+    super.initState();
+  }   
+```
+ - If the activeIndex matches the index of the image, its opacity is set to 1 (visible), otherwise the opacity is set to 0 (hidden).
+ ```
+    Positioned(
+      // make image in center of Container with height: 350
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: AnimatedOpacity(
+        //If the activeIndex matches the index of the image,
+        //its opacity is set to 1 (visible), otherwise,
+        // the opacity is set to 0 (hidden).
+        opacity: activeIndex == 0 ? 1 : 0,
+        // the duration of the opacity animation,
+        duration: Duration(seconds: 4),
+        curve: Curves.linear,
+        // make borderRaius for this image
+        // child: ClipRRect(
+        //   borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          'assets/images/img_rengoku_ruc_chay.jpeg',
+          fit: BoxFit.cover,
+        ),
+      ),
+      // ),
+    ),
+ ```
+
+ - Make button
+ ```
+    Container(
+      padding: const EdgeInsets.only(
+        top: 3,
+        left: 3,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        // color: Colors.amber,
+        border: Border.all(
+          color: Colors.black,
+        ),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        height: 50,
+        color: Colors.amber,
+        child: Text(
+          "Login",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 75),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    ),
+ ```
--- lesson61_register_with_phone/lib/page/login.dart
+++ lesson61_register_with_phone/lib/page/login.dart
@@ -14,10 +14,14 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void initState() {
+    //Timer.periodic is a class used to create a periodic timer.
+    // It allows you to perform an action
+    //after a certain amount of time and repeat it periodically.
     Timer.periodic(Duration(seconds: 5), (timer) {
+      // after 5 seconds increase 1
       setState(() {
         activeIndex++;
-
+// count up to 4 => reset
         if (activeIndex == 4) activeIndex = 0;
       });
     });
@@ -38,142 +42,190 @@ class _LoginPageState extends State<LoginPage> {
             ),
             Container(
               height: 350,
-              child: Stack(children: [
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: AnimatedOpacity(
-                    opacity: activeIndex == 0 ? 1 : 0,
-                    duration: Duration(
-                      seconds: 1,
+              width: 350,
+              //make border Radius for the whole Child in Stack
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      // make image in center of Container with height: 350
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        //If the activeIndex matches the index of the image,
+                        //its opacity is set to 1 (visible), otherwise,
+                        // the opacity is set to 0 (hidden).
+                        opacity: activeIndex == 0 ? 1 : 0,
+                        // the duration of the opacity animation,
+                        duration: Duration(seconds: 4),
+                        curve: Curves.linear,
+                        // make borderRaius for this image
+                        // child: ClipRRect(
+                        //   borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          'assets/images/img_rengoku_ruc_chay.jpeg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // ),
                     ),
-                    curve: Curves.linear,
-                    child: Image.network(
-                      'https://ouch-cdn2.icons8.com/As6ct-Fovab32SIyMatjsqIaIjM9Jg1PblII8YAtBtQ/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTg4/LzNmMDU5Mzc0LTky/OTQtNDk5MC1hZGY2/LTA2YTkyMDZhNWZl/NC5zdmc.png',
-                      height: 400,
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 1 ? 1 : 0,
+                        // duration for hide this image
+                        duration: Duration(seconds: 4),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          'assets/images/img_rengoku_in_fire_9th.jpeg',
+                          // height: 350,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: AnimatedOpacity(
-                    opacity: activeIndex == 1 ? 1 : 0,
-                    duration: Duration(seconds: 1),
-                    curve: Curves.linear,
-                    child: Image.network(
-                      'https://ouch-cdn2.icons8.com/vSx9H3yP2D4DgVoaFPbE4HVf6M4Phd-8uRjBZBnl83g/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNC84/MzcwMTY5OS1kYmU1/LTQ1ZmEtYmQ1Ny04/NTFmNTNjMTlkNTcu/c3Zn.png',
-                      height: 400,
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 2 ? 1 : 0,
+                        // duration for hide this image
+                        duration: Duration(seconds: 4),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          'assets/images/img_inosuke_moon.jpeg',
+                          // height: 350,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: AnimatedOpacity(
-                    opacity: activeIndex == 2 ? 1 : 0,
-                    duration: Duration(seconds: 1),
-                    curve: Curves.linear,
-                    child: Image.network(
-                      'https://ouch-cdn2.icons8.com/fv7W4YUUpGVcNhmKcDGZp6pF1-IDEyCjSjtBB8-Kp_0/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMTUv/ZjUzYTU4NDAtNjBl/Yy00ZWRhLWE1YWIt/ZGM1MWJmYjBiYjI2/LnN2Zw.png',
-                      height: 400,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: AnimatedOpacity(
-                    opacity: activeIndex == 3 ? 1 : 0,
-                    duration: Duration(seconds: 1),
-                    curve: Curves.linear,
-                    child: Image.network(
-                      'https://ouch-cdn2.icons8.com/AVdOMf5ui4B7JJrNzYULVwT1z8NlGmlRYZTtg1F6z9E/rs:fit:784:767/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTY5/L2NlMTY1MWM5LTRl/ZjUtNGRmZi05MjQ3/LWYzNGQ1MzhiOTQ0/Mi5zdmc.png',
-                      height: 400,
-                    ),
-                  ),
-                )
-              ]),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextField(
-              cursorColor: Colors.black,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(0.0),
-                labelText: 'Email',
-                hintText: 'Username or e-mail',
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w400,
-                ),
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14.0,
-                ),
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.black,
-                  size: 18,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                floatingLabelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 1.5),
-                  borderRadius: BorderRadius.circular(10.0),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 3 ? 1 : 0,
+                        // duration for hide this image
+                        duration: Duration(seconds: 4),
+                        curve: Curves.linear,
+                        child: Image.network(
+                          'https://i.pinimg.com/564x/dc/95/33/dc953375d20e245b57bef1a38ed86ec6.jpg',
+                          // height: 350,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 40,
             ),
+            // make textfield when user presson on this ->
+            // show label and hintText, but the label needs
+            // to line on the border of textField
+
+            // email
             TextField(
+              // change color of cursor when use click on this field
               cursorColor: Colors.black,
               decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(0.0),
-                labelText: 'Password',
-                hintText: 'Password',
-                hintStyle: TextStyle(
+                // contentPadding: const EdgeInsets.symmetric(
+                //   vertical: 10,
+                //   horizontal: 10,
+                // ),
+                hintText: 'Username or Email',
+                hintStyle: const TextStyle(
                   color: Colors.grey,
-                  fontSize: 14.0,
+                  fontSize: 14,
                 ),
+                prefixIcon: const Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                border: InputBorder.none,
+                // don't touch into it
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.green.shade400,
+                  ),
+                  borderRadius: BorderRadius.circular(
+                    20,
+                  ),
+                ),
+                // when touch into this textField
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.amber.shade800,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                // hint Label
+                labelText: 'Email',
+                // modify labelText when it is pressed on
+                // line on borderSide
+                floatingLabelStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                // modify style for label
                 labelStyle: TextStyle(
                   color: Colors.black,
                   fontSize: 14.0,
-                  fontWeight: FontWeight.w400,
                 ),
-                prefixIcon: Icon(
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            TextField(
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                // make size for this textField
+                contentPadding: const EdgeInsets.all(0),
+                labelText:
+                    'Password', // make label show in border of text field
+                hintText: 'Password',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.0,
+                ),
+                labelStyle: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w700,
+                ),
+                prefixIcon: const Icon(
                   Icons.key,
                   color: Colors.black,
                   size: 18,
                 ),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade200,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                floatingLabelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                ),
+                // floatingLabelStyle: TextStyle(
+                //   color: Colors.black,
+                //   fontSize: 18.0,
+                // ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 1.5),
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.black,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
               ),
             ),
@@ -185,9 +237,10 @@ class _LoginPageState extends State<LoginPage> {
                   child: Text(
                     'Forgot Password?',
                     style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400),
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 )
               ],
@@ -195,22 +248,40 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(
               height: 30,
             ),
-            MaterialButton(
-              onPressed: () {},
-              height: 45,
-              color: Colors.black,
-              child: Text(
-                "Login",
-                style: TextStyle(color: Colors.white, fontSize: 16.0),
+            // make button login
+            Container(
+              padding: const EdgeInsets.only(
+                top: 3,
+                left: 3,
               ),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                // color: Colors.amber,
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                height: 50,
+                color: Colors.amber,
+                child: Text(
+                  "Login",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 75),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
+            // don't have account
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -225,10 +296,10 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () {},
                   child: Text(
                     'Register',
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.blue,
                       fontSize: 14.0,
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w800,
                     ),
                   ),
                 )
